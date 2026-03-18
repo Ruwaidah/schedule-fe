f394c41b67f896ff8c9f2013251b144deef1fe89
Ruwaidah Alfakhri <ruwaidah.riyadh@gmail.com>
2026-03-17 21:01:06 -0500

    cleaning code

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './app/store.js'
-import { BrowserRouter } from 'react-router-dom'
 import AppBootstrap from './AppBootstrap.jsx'
 
 createRoot(document.getElementById('root')).render(
